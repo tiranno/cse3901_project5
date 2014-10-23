649473f822f5dddd4edc3b4d0f33800615df6096
Tyler Pedelose <pedelose.1@osu.edu>
2014-10-23 15:55:20 -0400

    Added basic functions and outlines
    Still need to add more complex functions and memory operations
    NOTE: "-" is both minus and subtract. Remember this on parsing the input.

---
+++ js/calculator.js
@@ -0,0 +1,48 @@
+//storage var
+//length var
+
+//adds x to y and returns the result
+function add (x, y) {
+  return (x+y);
+}
+
+//subtracts y from x and returns the result
+function subtract (x, y) {
+  return (x-y);
+}
+
+//multiplies x to y and returns the result
+function multiply (x, y) {
+  return (x * y);
+}
+
+//divides x to y and returns the result
+function divide (x, y) {
+  
+}
+
+//raises x to the yth power and returns the result
+function power (x, y) {
+  
+}
+
+//finds the square root of x and returns the result
+function root_2 (x) {
+  
+}
+
+function memory_clear () {
+  
+}
+
+function memory_save () {
+  
+}
+
+function memory_recall () {
+  
+}
+
+function memory_add () {
+  
+}
