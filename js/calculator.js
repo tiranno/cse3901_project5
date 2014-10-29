fe21486468b18a9e5455c16e4bb24d1542988ba6
ph00l3ry <ph00l3ry@gmail.com>
2014-10-28 21:27:44 -0400

    Implemented square + power

--- js/calculator.js
+++ js/calculator.js
@@ -1,40 +1,73 @@
-var counter=0;
-function input(val){
-	document.getElementById('input').innerHTML+=val;
+var inputCount = 0;
+var operators = ['+', '-', '*', '/', '(', ')'], input = [];
 
+Array.prototype.contains = function ( tmp ) {
+   for (i in this) {
+       if (this[i] == tmp) return true;
+   }
+   return false;
 }
+
+function addToInput(val){
+	if(operators.contains(val)){
+		inputCount++;
+		input[inputCount] = val;
+		inputCount++;
+	}
+	else{
+		if(input[inputCount] != null){
+			input[inputCount] += val;	
+		}
+		else{
+			input[inputCount] = val;
+		}
+	}
+	document.getElementById('input').innerHTML += val;
+}
+
 function del() {
 	var temp=document.getElementById('input').innerHTML;
 	temp=temp.substring(0,temp.length-1);
 	document.getElementById('input').innerHTML=temp;
 }
+
 function divide() {
 	var temp=document.getElementById('input').innerHTML;
 	temp=temp;
 	document.getElementById('input').innerHTML=temp;
 }
-function evalinput() {
-	var temp=document.getElementById('input').innerHTML;
-	document.getElementById('input').innerHTML=eval(document.getElementById('input').innerHTML);
-	document.getElementById('result').innerHTML=temp+'='+document.getElementById('input').innerHTML;
-	document.getElementById('history').innerHTML+="<div id=\"history"+counter+"\">"+temp+'='+document.getElementById('input').innerHTML+"<button OnClick=\"input("+document.getElementById('input').innerHTML+")\">INPUT</button>" + "</div>";
-	counter+=1;
 
+function evalinput() {
+	var tmp = input.join('');
+	document.getElementById('result').innerHTML = eval(tmp);
+
+	// var temp=document.getElementById('input').innerHTML;
+	// document.getElementById('input').innerHTML=eval(document.getElementById('input').innerHTML);
+	// document.getElementById('result').innerHTML=temp+'='+document.getElementById('input').innerHTML;
+	// document.getElementById('history').innerHTML+="<div id=\"history"+counter+"\">"+temp+'='+document.getElementById('input').innerHTML+"<button OnClick=\"input("+document.getElementById('input').innerHTML+")\">INPUT</button>" + "</div>";
+	// counter+=1;
 }
+
 function memclear() {
 	document.getElementById('memory').innerHTML="clear";
 }
+
 function memstore() {
 	document.getElementById('memory').innerHTML=document.getElementById('input').innerHTML;
 }
+
 function memrecall() {
 	document.getElementById('input').innerHTML+=document.getElementById('memory').innerHTML;
 }
+
 function clearcalc() {
+	input.length = 0;
+	document.getElementById('history').innerHTML = document.getElementById('input').innerHTML;
 	document.getElementById('input').innerHTML="";
 	document.getElementById('result').innerHTML="";
-	document.getElementById('history').innerHTML="";
+	document.getElementById('power').style.display = "none";
 }
+
 //storage var
 //length var
 
@@ -68,22 +101,45 @@ function root_2 (x) {
   
 }
 
-function squared () {
-	var operators = ['+', '-', '*', '/'];
-	var i=0;
-	var lastind=0;
-	var temp=document.getElementById('input').innerHTML;
-	var temparray=[1,2,3];
-	for(i=0;i<operators.length;i++){
-		if(temp.lastIndexOf(operators[i])>0 && temp.lastIndexOf(operators[i])>lastind){
-			lastind=temp.lastIndexOf(operators[i]);
-		}
-	}
-	document.getElementById('result').innerHTML=Math.pow(temp.substring(lastind+1,temp.length),2);
-	temp=temp.substring(0,lastind+1)+Math.pow(temp.substring(lastind+1,temp.length),2);
-	document.getElementById('input').innerHTML=temp;
-	//document.getElementById('result').innerHTML=Math.pow(temp,2);
+function exponentDisplay(){
+	document.getElementById('power').style.display = "block";
 }
+
+function power(exponent){
+	var operators = ['+', '-', '*', '/'], tmp = [], tmpNum;
+	if(exponent == 0){
+		exponent = document.getElementById('powNum').value;
+		document.getElementById('powNum').value = "";
+		document.getElementById('power').style.display = "none";
+	}
+	if(input[inputCount - 1] == ')'){
+
+	}
+	else{
+		tmpNum = parseInt(input[inputCount]);
+		tmpNum = Math.pow(tmpNum, exponent);
+	}
+	document.getElementById('input').innerHTML += "^" + exponent;
+	input[inputCount] = tmpNum;
+}
+
+// function squared () {
+// 	var operators = ['+', '-', '*', '/'];
+// 	var i=0;
+// 	var lastind=0;
+// 	var temp=document.getElementById('input').innerHTML;
+// 	var temparray=[1,2,3];
+// 	for(i=0;i<operators.length;i++){
+// 		if(temp.lastIndexOf(operators[i])>0 && temp.lastIndexOf(operators[i])>lastind){
+// 			lastind=temp.lastIndexOf(operators[i]);
+// 		}
+// 	}
+// 	document.getElementById('result').innerHTML=Math.pow(temp.substring(lastind+1,temp.length),2);
+// 	temp=temp.substring(0,lastind+1)+Math.pow(temp.substring(lastind+1,temp.length),2);
+// 	document.getElementById('input').innerHTML=temp;
+// 	//document.getElementById('result').innerHTML=Math.pow(temp,2);
+// }
+
 function memory_clear () {
   
 }
