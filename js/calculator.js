846a28fc9dc47a02a37669529d5a2e41e5111b6e
Tom Carlin <tmstripes@tan-carlins-macbook-pro.local>
2014-10-29 03:43:45 -0400

    fixed memory finished buttons besides trig

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,4 @@
 var inputCount = 0;
-var counter=0;
 var operators = ['+', '-', '*', '/', '(', ')'], input = [];
 
 Array.prototype.contains = function ( tmp ) {
@@ -43,15 +42,9 @@ function divide() {
 function evalinput() {
 	var tmp = input.join('');
 	document.getElementById('result').innerHTML = eval(tmp);
-	//document.getElementById('history').innerHTML = tmp;
-	var hist = "<div id=\"history"+counter+"\">"+tmp+'='+document.getElementById('result').innerHTML+"<button OnClick=\"addToInput("+document.getElementById('input').innerHTML+")\">INPUT</button>" + "</div>";
+	var hist = tmp+'='+document.getElementById('result').innerHTML+"<button OnClick=\"addToInput("+document.getElementById('input').innerHTML+")\">INPUT</button><br>";
 	hist += document.getElementById('history').innerHTML;
 	document.getElementById('history').innerHTML=hist;
-	// var temp=document.getElementById('input').innerHTML;
-	// document.getElementById('input').innerHTML=eval(document.getElementById('input').innerHTML);
-	// document.getElementById('result').innerHTML=temp+'='+document.getElementById('input').innerHTML;
-	// document.getElementById('history').innerHTML+="<div id=\"history"+counter+"\">"+tmp+'='+document.getElementById('input').innerHTML+"<button OnClick=\"addToInput("+document.getElementById('input').innerHTML+")\">INPUT</button>" + "</div>";
-	counter+=1;
 }
 
 function memclear() {
@@ -59,58 +52,32 @@ function memclear() {
 }
 
 function memstore() {
-	document.getElementById('memory').innerHTML = document.getElementById('input').innerHTML;
+	document.getElementById('memory').innerHTML = document.getElementById('result').innerHTML;
 }
 
 function memrecall() {
+	if(input[inputCount] != null){
+		input[inputCount] += document.getElementById('memory').innerHTML;
+	}
+	else{
+		input[inputCount] = document.getElementById('memory').innerHTML;
+	}
 	document.getElementById('input').innerHTML+=document.getElementById('memory').innerHTML;
-	input[inputCount] = document.getElementById('input').innerHTML;
+}
+
+function memplus() {
+	document.getElementById('memory').innerHTML=eval(document.getElementById('result').innerHTML+"+"+document.getElementById('memory').innerHTML);
 }
 
 function clearcalc() {
 	var hist="CLEAR<br>";
 	hist += document.getElementById('history').innerHTML;
 	document.getElementById('history').innerHTML=hist;
-	//document.getElementById('history').innerHTML += input.join('');
-	//document.getElementById('history').innerHTML += '<br>';
 	document.getElementById('input').innerHTML="";
 	document.getElementById('result').innerHTML="";
 	document.getElementById('power').style.display = "none";
 	input.length = 0;
 	inputCount = 0;
-}
-
-//storage var
-//length var
-
-//adds x to y and returns the result
-function add (x, y) {
-  return (x+y);
-}
-
-//subtracts y from x and returns the result
-function subtract (x, y) {
-  return (x-y);
-}
-
-//multiplies x to y and returns the result
-function multiply (x, y) {
-  return (x * y);
-}
-
-//divides x to y and returns the result
-function divide (x, y) {
-  
-}
-
-//raises x to the yth power and returns the result
-function power (x, y) {
-  
-}
-
-//finds the square root of x and returns the result
-function root_2 (x) {
-  
 }
 
 function exponentDisplay(){
@@ -135,23 +102,6 @@ function power(exponent){
 	input[inputCount] = tmpNum;
 }
 
-// function squared () {
-// 	var operators = ['+', '-', '*', '/'];
-// 	var i=0;
-// 	var lastind=0;
-// 	var temp=document.getElementById('input').innerHTML;
-// 	var temparray=[1,2,3];
-// 	for(i=0;i<operators.length;i++){
-// 		if(temp.lastIndexOf(operators[i])>0 && temp.lastIndexOf(operators[i])>lastind){
-// 			lastind=temp.lastIndexOf(operators[i]);
-// 		}
-// 	}
-// 	document.getElementById('result').innerHTML=Math.pow(temp.substring(lastind+1,temp.length),2);
-// 	temp=temp.substring(0,lastind+1)+Math.pow(temp.substring(lastind+1,temp.length),2);
-// 	document.getElementById('input').innerHTML=temp;
-// 	//document.getElementById('result').innerHTML=Math.pow(temp,2);
-// }
-
 function percent () {
 	var operators = ['+', '-', '*', '/'], tmp = [], tmpNum;
 	if(input[inputCount - 1] == ')'){
@@ -159,24 +109,9 @@ function percent () {
 	}
 	else{
 		tmpNum = parseInt(input[inputCount])/100;
-		//tmpNum = Math.pow(tmpNum, exponent);
 	}
 	document.getElementById('input').innerHTML += "%";
 	input[inputCount] = tmpNum;
 }
 
-function memory_clear () {
-  
-}
 
-function memory_save () {
-  
-}
-
-function memory_recall () {
-  
-}
-
-function memory_add () {
-  
-}
